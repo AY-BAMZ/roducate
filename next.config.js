44ecb55322a4eb29d86c487b008c9ad99a872451
ayobami paul <aybamz181@gmail.com>
2023-12-06 16:54:13 +0100

    fixing dev dependencies

--- next.config.js
+++ next.config.js
@@ -5,11 +5,11 @@ const nextConfig = {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
-      },
+    },
     webpack(config) {
         // Grab the existing rule that handles SVG imports
         const fileLoaderRule = config.module.rules.find((rule) =>
-            rule.test?.test?.(".svg")
+            rule.test?.test?.('.svg')
         );
 
         config.module.rules.push(
@@ -24,7 +24,7 @@ const nextConfig = {
                 test: /\.svg$/i,
                 issuer: /\.[jt]sx?$/,
                 resourceQuery: { not: /url/ }, // exclude if *.svg?url
-                use: ["@svgr/webpack"],
+                use: ['@svgr/webpack'],
             }
         );
 
